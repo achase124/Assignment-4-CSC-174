a49229a997d094400d5872693bef12119a1a0dfc
jpmurphy710 <jpmurphy710@gmail.com>
2019-10-05 04:07:11 -0400

    FInished filling out HTML

--- odd.php
+++ odd.php
@@ -14,7 +14,22 @@
 		</header>
 
 		<main>
-
-			<h2>The Odd "Student" Out</h2>
-			
+			<h2>The Odd Student Out: Kat Fuss</h2>
+			<p>Kat Fuss is a Rochester native, unlike Ana and Andrew, who were both born outside mainland United States and resided there for some time.</p>
 		</main>
+
+		<aside>
+			<img>
+		</aside>
+
+		<article>
+			<h3>Early Life</h3>
+				<p>Kat Fuss has forever been a resident of Rochester, having attended gradeschool here and classes suited to her aptitude for hands on, practical art-based opportunities, which led to her developing a taste for theatre and other art-based technicalities.</p>
+			<h3>Interests</h3>
+				<p>Kat has developed an interest in theatre through her exploits in high school, as well as an interst in CAD and PhotoShop, which she claims is what sparked her interest in the DMS major, and is something that she sees as having practical value in the real world.</p>
+			<h3>Major</h3>
+				<p>Kat is presently a junior hoping to graduate in 2021 with a BA in DMS and a minor in Spanish. Kat is taking classes this semester in particular which are outside her usual field of study, such as The Art of Film and Modern Architecture, but offer a means of opening her eyes to the full spectrum of DMS as a whole.</p>
+
+		</article>
+	</body>
+</html>
