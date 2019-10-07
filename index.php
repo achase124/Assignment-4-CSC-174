47422e8b08af3242f75c409082718659d4394e3a
joshwolk <joshuawolkoff@gmail.com>
2019-10-07 16:14:33 -0400

    put all students on same page

--- index.php
+++ index.php
@@ -20,37 +20,10 @@
 			</ul>
 		</nav>
 
+		
 		<main>
-			<h2>The Similarity</h2>
-			<p>Neither Ana Acuña nor Andrew Thankson were born in mainland United States, and thus will be the topic of similarity between these two students.</p>
+			<h2>The Odd Student Out: Kat Fuss</h2>
+			<p>Kat Fuss is a Rochester native, unlike Ana and Andrew, who were both born outside mainland United States and resided there for some time.</p>
 		</main>
-
-		<aside>
-			<img src="img/ana.jpg" alt="Ana Acuña">
-		</aside>
-
-		<article>
-			<h2>Ana Acuña</h2>
-				<h3>Early Life</h3>
-					<p>Ana was born in Mayagüez, Puerto Rico, quite the change from Rochester, NY. Evidently there is actually sun in Puerto, contrary to the frozen hell that is Rochester most of the year. During her younger years, Ana spent a lot of time at the campus which her parents both worked, thus helping her drive to be a stellar student, eventually winding up at the University of Rochester.</p>
-				<h3>Interests</h3>
-					<p>Ana is presently the President of the Gamma Phi Beta sorority, as well as having served time as the Financial Vice President of GPB. This has been a major plus to her time at UR because joining a sorority served as a means for meeting many new people, some being her closest friends today.</p>
-				<h3>Major</h3>
-					<p>After shuffling around with a few different majors, Ana finally settled on majoring in Business, namely Marketing. She feels that this is the best fit for her right now and that it sparked her journey to reach out and actually find out where her interests lie.</p>
-		</article>
-
-		<aside>
-			<img src="img/andrew.jpeg">
-		</aside>
-
-		<article>
-			<h2>Andrew Thankson</h2>
-				<h3>Early Life</h3>
-					<p>Andrew Thankson was born in Aburi, a small town in the Eastern region of Ghana. He went to school in this town up until the completion of junior high, at which time he went to a boarding school. During boarding school he studied general science in preparation for college, which was a tough decision between studying in Ghana or studying abroad.</p>
-				<h3>Interests</h3>
-					<p>During his freshman year of college, Andrew acted as an EcoRep through UR Reslife. He joined the National Society of Black Engineers and the Pan-African Student Association, and Philosophy Council as well, maintaining a healthy presence in student organizations.</p>
-				<h3>Major</h3>
-					<p>Andrew decided, following taking classes in high school, to switch his intended major from electical engineering. Andrew hopes to graduate in 2022 with a BS in CS and minors in Philosophy and Psychology.</p>
-		</article>
 	</body>
 </html>
