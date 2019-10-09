8e4b00813087635b52506e79b7c4955ab477379c
Alex Chase <55210806+achase124@users.noreply.github.com>
2019-10-08 22:19:20 -0400

    Homepage including Full-Page JQuery Slideshow

--- index.php
+++ index.php
@@ -1,42 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-	<meta charset="utf-8">
-	<title>Team Berlin || Assignment 04: Odd Student Out</title>
-	<link rel="stylesheet" href="css/z-pattern.css">
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>Team Berlin || Assignment 04: Odd Student Out</title>
+		<link rel="stylesheet" href="css/z-pattern.css">
+		<!-- Slideshow ... -->
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+		<script src="js/fullclip.js"></script>
+		<!-- ... -->
 
-<body>
+	</head>
 
-	<div class="container">
+	<body class="fullBackground"><!-- Class to activate background slider -->
 
-		<img class="tl" src="img/icon.png">
+		<div class="container">
 
-		<div class="middle">
-			<h1>Team Berlin</h1>
+			<img class="tl" src="img/icon.png">
+
+			<div class="middle">
+				<h1>Team Berlin</h1>
+			</div>
+
+
+			<div class="tr">
+				<nav>
+					<a href="https://en.wikipedia.org/wiki/Berlin">Learn more about Berlin</a>
+				</nav>
+			</div>
+
+
+			<div class="bl">
+				<section>
+					<p>Difference: Kat Fuss is a Rochester native, unlike Ana and Andrew, who were both born outside mainland United States and resided there for some time.</p>
+				</section>
+			</div>
+
+			<div class="br">
+				<nav>
+					<a href="odd.php">Students</a>
+				</nav>
+			</div>
+
 		</div>
 
-		<div class="tr">
-			<nav>
-				<a href="https://en.wikipedia.org/wiki/Berlin">Learn more about Berlin</a>
-			</nav>
-		</div>
+		<script>
+				$('.fullBackground').fullClip({
+			  images: ['img/brandenburg_gate.jpg','img/bridge.jpg','img/skyline.jpg']
+			});
+		</script>
 
+	</body>
 
-		<div class="bl">
-			<section>
-				<p>Difference: Kat Fuss is a Rochester native, unlike Ana and Andrew, who were both born outside mainland United States and resided there for some time.</p>
-			</section>
-		</div>
-
-		<div class="br">
-			<nav>
-				<a href="odd.php">Students</a>
-			</nav>
-		</div>
-
-	</div>
-
-	
-</body>
+</html>
